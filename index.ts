8e62660aaf8aa738bec65677e5432b1755764a2f
coderhyh <772567615@qq.com>
2022-06-29 00:54:35 +0800

    群聊

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const app: Express = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://127.0.0.1:3000","http://localhost:3000"]
+    origin: ["http://127.0.0.1:3000","http://localhost:3000", "https://chat.coderhyh.top"]
   }
 })
 require('./modules/socket')(io)
--- modules/socket.ts
+++ modules/socket.ts
@@ -1,11 +1,12 @@
-import { Server } from 'socket.io'
+import { Server } from "socket.io";
 
 module.exports = (io: Server) => {
-  io.on('connection', (socket) => {
-    socket.emit('connectSuccess', '连接成功')
-    socket.on('hehe', (e: string) => {
-      console.log(e);
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.emit("connectSuccess", "连接成功");
+    // 广播
+    socket.on("sendMsg", (e: string) => {
+      socket.broadcast.emit("sendMsg", e);
     });
-    console.log('a user connected');
   });
-}
+};
