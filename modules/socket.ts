4acc01bda87ab2b97e9e0f1e67f2e66f780443b4
hyh <772567615@qq.com>
2022-06-29 17:37:19 +0800

    master

--- modules/socket.ts
+++ modules/socket.ts
@@ -1,12 +1,19 @@
 import { Server } from "socket.io";
-
+interface SendData {
+  msg: string
+  name: string
+  right: boolean
+  date: string
+}
 module.exports = (io: Server) => {
   io.on("connection", (socket) => {
     console.log("a user connected");
     socket.emit("connectSuccess", "连接成功");
     // 广播
     socket.on("sendMsg", (e: string) => {
-      socket.broadcast.emit("sendMsg", e);
+      const sendData: SendData = JSON.parse(e);
+      sendData.right = false;
+      socket.broadcast.emit("sendMsg", sendData);
     });
   });
 };
